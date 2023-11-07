5060736efb498a4a95807ff2698c22960965b031
Hieu Nguyen <41591244+hieunguyen1053@users.noreply.github.com>
2023-11-07 14:57:24 +0000

    Create prepare_tokenizer.py

---
+++ prepare_tokenizer.py
@@ -0,0 +1,11 @@
+import argparse
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model-name", help="Name of the model", required=True)
+parser.add_argument("--output-path", help="Path to save the model", required=True)
+args = parser.parse_args()
+
+tokenizer = AutoTokenizer.from_pretrained("vietgpt/dama-2-7b-chat")
+
+tokenizer.save_pretrained(args.output_path)
