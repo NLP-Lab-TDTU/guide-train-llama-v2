95bd978bbb46cf7e000e41a79593bf1100672209
Hieu Nguyen <41591244+hieunguyen1053@users.noreply.github.com>
2023-11-07 14:54:20 +0000

    Update prepare_model.py

--- prepare_model.py
+++ prepare_model.py
@@ -6,7 +6,7 @@ parser.add_argument("--model-name", help="Name of the model", required=True)
 parser.add_argument("--output-path", help="Path to save the model", required=True)
 args = parser.parse_args()
 
-tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+tokenizer = AutoTokenizer.from_pretrained("vietgpt/dama-2-7b-chat")
 model = AutoModelForCausalLM.from_pretrained(args.model_name)
 
 tokenizer.save_pretrained(args.output_path)
