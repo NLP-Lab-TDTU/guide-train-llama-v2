91cd81e563ac265855e1c80030db289bfd6a94ee
Hieu Nguyen <41591244+hieunguyen1053@users.noreply.github.com>
2023-11-07 02:25:03 +0000

    Update prepare_dataset.py

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,32 +1,53 @@
 from datasets import load_dataset, concatenate_datasets, DatasetDict, Dataset
+from transformers import AutoTokenizer
 
-list_datasets = [
-    "vietgpt/openwebtext_en",
-    "nlplabtdtu/webtext_cc_en",
-    "nlplabtdtu/cc-2014-52",
-    "nlplabtdtu/cc-2015-48",
-    "nlplabtdtu/cc-2016-50",
-    "nlplabtdtu/cc-2017-51",
-    "nlplabtdtu/cc-2018-51",
-    "nlplabtdtu/cc-2019-51",
-    "nlplabtdtu/cc-2020-51",
-    "nlplabtdtu/cc-2021-49",
-    "nlplabtdtu/cc-2022-40",
-    "vietgpt/the_pile_openwebtext2",
-    "vietgpt/c4_vi",
-    "vietgpt/OSCAR-2109",
-    "vietgpt/OSCAR-2201",
-    "vietgpt/OSCAR-2301"
-]
+# DEFAULT_TEMPLATE = """{% for message in messages %}{% if message['role'] == 'user' %}{{ bos_token + '[INST] ' + message['content'] + ' [/INST]' }}{% elif message['role'] == 'assistant' %}{{ ' '  + message['content'] + ' ' + eos_token }}{% endif %}{% endfor %}"""
+DEFAULT_TEMPLATE = """{% if messages[0]['role'] == 'system' %}{{ bos_token + '[INST] ' + '<<SYS>>\\n' + messages[0]['content'] + '\\n<</SYS>>\\n\\n' + messages[1]['content'] + ' [/INST]' }}{% for message in messages[:2] %}{% if message['role'] == 'user' %}{{ bos_token + '[INST] ' + message['content'] + ' [/INST]' }}{% elif message['role'] == 'assistant' %}{{ ' '  + message['content'] + ' ' + eos_token }}{% endif %}{% endfor %}{% else%}{% for message in messages %}{% if message['role'] == 'user' %}{{ bos_token + '[INST] ' + message['content'] + ' [/INST]' }}{% elif message['role'] == 'assistant' %}{{ ' '  + message['content'] + ' ' + eos_token }}{% endif %}{% endfor %}{% endif %}"""
+
+tokenizer = AutoTokenizer.from_pretrained("vietgpt/dama-2-7b-chat", verbose=False)
+tokenizer.chat_template = DEFAULT_TEMPLATE
+tokenizer.pad_token = '<pad>'
 
 train_datasets = Dataset.from_dict({'text': []})
 valid_datasets = Dataset.from_dict({'text': []})
 
-for dataset_name in list_datasets:
-    train_dataset = load_dataset(dataset_name, split="train[1000:]", num_proc=128)
+list_datasets = [
+    ("vietgpt/ultrachat", "train_2048"),
+    ("vietgpt/orca", "train_2048"),
+    ("vietgpt/WizardLM_evol_instruct_V2_196k", "train_2048"),
+    
+    ("vietgpt/databricks_dolly15k_en", "train"),
+    ("vietgpt/databricks_dolly15k_vi", "train"),
+    ("vietgpt/alpaca_en", "train"),
+    ("vietgpt/alpaca_vi", "train"),
+
+    ("vietgpt/grade_school_math", "train"),
+    ("vietgpt/webglm-qa", "train"),
+]
+
+for dataset_name, dataset_split in list_datasets:
+    train_dataset = load_dataset(dataset_name, split=f"{dataset_split}[1000:]", num_proc=128)
+    train_dataset = train_dataset.map(lambda x: {'text': tokenizer.apply_chat_template(x['messages'], tokenize=False)}, num_proc=64, remove_columns=train_dataset.column_names)
     train_dataset = train_dataset.remove_columns([c for c in train_dataset.column_names if c != 'text'])
     
-    valid_dataset = load_dataset(dataset_name, split="train[:1000]", num_proc=128)
+    valid_dataset = load_dataset(dataset_name, split=f"{dataset_split}[:1000]", num_proc=128)
+    valid_dataset = valid_dataset.map(lambda x: {'text': tokenizer.apply_chat_template(x['messages'], tokenize=False)}, num_proc=64, remove_columns=valid_dataset.column_names)
+    valid_dataset = valid_dataset.remove_columns([c for c in valid_dataset.column_names if c != 'text'])
+
+    train_datasets = concatenate_datasets([train_datasets, train_dataset])
+    valid_datasets = concatenate_datasets([valid_datasets, valid_dataset])
+    
+list_datasets = [
+    ("vietgpt/basic", "train"),
+]
+
+for dataset_name, dataset_split in list_datasets:
+    train_dataset = load_dataset(dataset_name, split=f"{dataset_split}", num_proc=128)
+    train_dataset = train_dataset.map(lambda x: {'text': tokenizer.apply_chat_template(x['messages'], tokenize=False)}, num_proc=64, remove_columns=train_dataset.column_names)
+    train_dataset = train_dataset.remove_columns([c for c in train_dataset.column_names if c != 'text'])
+    
+    valid_dataset = load_dataset(dataset_name, split=f"{dataset_split}", num_proc=128)
+    valid_dataset = valid_dataset.map(lambda x: {'text': tokenizer.apply_chat_template(x['messages'], tokenize=False)}, num_proc=64, remove_columns=valid_dataset.column_names)
     valid_dataset = valid_dataset.remove_columns([c for c in valid_dataset.column_names if c != 'text'])
 
     train_datasets = concatenate_datasets([train_datasets, train_dataset])
@@ -39,4 +60,4 @@ datasets = DatasetDict({
 
 datasets = datasets.shuffle(seed=42)
 
-datasets.save_to_disk('my_dataset1', num_proc=128)
+datasets.save_to_disk('my_sft_dataset', num_proc=128)
