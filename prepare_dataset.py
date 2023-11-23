a765ac91eb0d5ca03d181b799f8cae6c2ab88d69
Hieu Nguyen <41591244+hieunguyen1053@users.noreply.github.com>
2023-11-23 07:27:39 +0700

    Update prepare_dataset.py

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,42 +1,25 @@
 from datasets import load_dataset, concatenate_datasets, DatasetDict, Dataset
 
 list_datasets = [
-    "vietgpt/openwebtext_en",
-    "nlplabtdtu/webtext_cc_en",
-    "nlplabtdtu/cc-2014-52",
-    "nlplabtdtu/cc-2015-48",
-    "nlplabtdtu/cc-2016-50",
-    "nlplabtdtu/cc-2017-51",
-    "nlplabtdtu/cc-2018-51",
-    "nlplabtdtu/cc-2019-51",
-    "nlplabtdtu/cc-2020-51",
-    "nlplabtdtu/cc-2021-49",
-    "nlplabtdtu/cc-2022-40",
-    "vietgpt/the_pile_openwebtext2",
-    "vietgpt/c4_vi",
-    "vietgpt/OSCAR-2109",
-    "vietgpt/OSCAR-2201",
-    "vietgpt/OSCAR-2301"
+    "vietgpt/arxiv",
+    "vietgpt/github",
+    "vietgpt/open-web-math",
+    "vietgpt/stackexchange",
+    "vietgpt/CulturaX",
 ]
 
 train_datasets = Dataset.from_dict({'text': []})
-valid_datasets = Dataset.from_dict({'text': []})
 
 for dataset_name in list_datasets:
-    train_dataset = load_dataset(dataset_name, split="train[1000:]", num_proc=128)
+    train_dataset = load_dataset(dataset_name, split="train", num_proc=128)
     train_dataset = train_dataset.remove_columns([c for c in train_dataset.column_names if c != 'text'])
-    
-    valid_dataset = load_dataset(dataset_name, split="train[:1000]", num_proc=128)
-    valid_dataset = valid_dataset.remove_columns([c for c in valid_dataset.column_names if c != 'text'])
 
     train_datasets = concatenate_datasets([train_datasets, train_dataset])
-    valid_datasets = concatenate_datasets([valid_datasets, valid_dataset])
 
 datasets = DatasetDict({
     'train': train_datasets,
-    'validation': valid_datasets,
 })
 
 datasets = datasets.shuffle(seed=42)
 
-datasets.save_to_disk('my_dataset1', num_proc=128)
+datasets.save_to_disk('my_dataset2', num_proc=128)
