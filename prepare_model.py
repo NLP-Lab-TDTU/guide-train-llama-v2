32d136f3914ce9dbfe2c1efc4d53777987036001
Hieu Nguyen <41591244+hieunguyen1053@users.noreply.github.com>
2023-12-29 11:36:10 +0700

    Update prepare_model.py

--- prepare_model.py
+++ prepare_model.py
@@ -6,8 +6,8 @@ parser.add_argument("--model-name", help="Name of the model", required=True)
 parser.add_argument("--output-path", help="Path to save the model", required=True)
 args = parser.parse_args()
 
-tokenizer = AutoTokenizer.from_pretrained(args.model_name)
-model = AutoModelForCausalLM.from_pretrained(args.model_name)
+tokenizer = AutoTokenizer.from_pretrained(args.model_name, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(args.model_name, trust_remote_code=True)
 
 tokenizer.save_pretrained(args.output_path)
 model.save_pretrained(args.output_path)
